c775b9cdf4244d9e4ac5f0aafbf940a4e4ad875f
ayushiprasad3120-afk <ayushiprasad3120@gmail.com>
2026-08-12 02:52:44 +0530

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://www.insuredirect.com",
-  generateRobotsTxt: false, // handled by app/robots.ts
-  exclude: ["/campaign/*", "/api/*"],
-  changefreq: "weekly",
-};
